34371472b4a6c51307eb4a2e2f0ae375d75ca6b7
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-12-29 11:54:40 +0530

    Revert "updated API route"
    This reverts commit 7756ef5284fc9d25d23e7bd6570f3c0089227517.

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = true;
 const api = axios.create({
 	// baseURL: "https://doubt-overflow-api.herokuapp.com/api/v1",
 	// baseURL: "http://localhost:8000/api/v2",
-	baseURL: "https://track-it-buxj.onrender.com/api/v2",
+	baseURL: "https://track-it-v2.herokuapp.com/api/v2",
 });
 
 export const login = (authData) => api.post("/users/login", authData);
